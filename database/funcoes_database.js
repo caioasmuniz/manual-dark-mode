41a669d4939a4a570ffeef61f51f3a8495a0a8a3
Felipe Sato <43546131+Felipe-Sato@users.noreply.github.com>
2021-05-04 01:43:42 -0300

    Update funcoes_database.js
    Acredito que a forma de inicializar o FirebaseApp está errado, por favor corrijam se for o caso!
    Os métodos do Firebase não estão funcionando e precisam de mais testes para simular o BD.

--- database/funcoes_database.js
+++ database/funcoes_database.js
@@ -1,99 +1,97 @@
-/* ------------------------ INICIALIZAÇÃO DO FIRESTORE ----------------------- */
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-    apiKey: '### FIREBASE API KEY ###',
-    authDomain: '### FIREBASE AUTH DOMAIN ###',
-    projectId: '### CLOUD FIRESTORE PROJECT ID ###'
-});
-    var db = firebase.firestore();
+const firebaseConfig = require("../config/firebase-config");
+/* ---------- INITIALIZING FIRESTORE ----------- */
+let require 
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
 
-/* ------------------------- AUTENTICAÇÃO DO USUARIO ------------------------- */
-// Pegando dados pelos campos do formulario do site
-var nome_add = document.getElementById('nome');
-var tema_add = document.getElementById('tema');
-var url_add = document.getElementById('site');
-var css_add = document.getElementById('codigo');
+/* ---------------- */
+var doc_name = document.getElementById("nome");
+var doc_filter = document.getElementById("tema");
+var doc_url = document.getElementById("url");
+var doc_css = document.getElementById("codigo");
 
-// Reconhecendo pedido de criacao de conta 
-function createRequest() {
-    // Procedimento de login
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then((user) => {
-            var credential = result.credential;
-            var token = credential.accessToken;
-            var user = result.user;
-        })
-        .catch((error) => {
-            console.log("Erro ao realizar o login!");
-            var errorCode = error.code;
-            var errorMessage = error.message;
-        });
-}
-
-// Login do usuario autenticado
+/* ---------------- USER AUTHENTICATION ------------------ */
+// Login Request
 function loginRequest() {
-    // Acoes do usuario autenticado
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
             var author = firebase.Auth().currentUser;
-            // Update um documento somente se o autor esta autenticado
-            function update(nome_doc) {
-                return db.collection("Library").doc(nome_doc.toString()).where("author", "==", author.uid).update({
-                    nome: nome_add,
-                    tema: tema_add,
-                    url: url_add,
-                    css: css_add,
-                    author: firebase.auth().currentUser.uid,
+            
+            // User Only Functions - INSERT
+            function insert(doc_name) {
+                return db.collection("Library").doc(doc_name.toString()).set({
+                    nome: doc_name,
+                    tema: doc_filter,
+                    url: doc_url,
+                    css: doc.css,
+                    author: author.uid(),
                     time: firebase.timestamp.now()
-                }) .then(() => {
-                    console.log("Arquivo atualizado com sucesso!");
-                }) .catch((error) => {
-                    console.log("Erro ao atualizar arquivo!")
+                })  .then (() => {
+                    console.log("Arquivo criado com sucesso!");
+                }) .catch ((error) => {
+                    console.log("Erro ao criar o arquivo!");
                     var errorCode = error.code;
                     var errorMessage = error.message;
+                    console.log('Codigo do erro: ', +errorCode);
                 })
             }
 
-            function remove(nome_doc) {
-                db.collection("Library").doc(nome_doc.toString()).where("author", "==", author.uid).delete()
-                .then(() => {
+            // User Only Functions - UPDATE
+            function update(doc_name) {
+                return db.collection("Library").doc(doc_name.toString()).where("author", "==", author.uid).update({
+                    nome: doc_name,
+                    tema: doc_filter,
+                    url: doc_url,
+                    css: doc_css,
+                    author: firebase.auth().currentUser.uid,
+                    time: firebase.timestamp.now()
+                }) .then (() => {
+                    console.log("Arquivo atualizado com sucesso!");
+                }) .catch ((error) => {
+                    console.log("Erro ao atualizar o arquivo!");
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    console.log('Codigo do erro: ', +errorCode);
+                })
+            }
+
+            // User Only Functions - REMOVE
+            function remove(doc_name) {
+                db.collection("Library").doc(doc_name.toString()).where("author", "==", author.uid).delete()
+                . then (() => {
                     console.log("Arquivo removido com sucesso!");
-                }) .catch((error) => {
+                }) .catch ((error) => {
                     console.log("Erro ao remover arquivo!");
                     var errorCode = error.code;
                     var errorMessage = error.message;
+                    console.log('Codigo do erro: ', +errorCode);
                 })
             }
-        // https://firebase.google.com/docs/reference/js/firebase.User
-        } 
-    });
+        }
+    })
 }
 
-// Saida do usuario
-firebase.auth().signOut()
-    .then(() => {
-        console.log("Logout realizadom com sucesso!");
-    })
-    .catch((error) => {
-        console.log("Erro ao realizar o logout, tente novamente!");
+// Logout Request
+function logoutRequest() {
+    firebase.auth().signOut()
+    . then (() => {
+        console.log("Logout realizado com sucesso!");
+    }) .catch ((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
+        console.log('Codigo do erro: ', +errorCode);
     })
+}
 
-// Adicionando dados
-function insere() {
-    db.collection("Library").doc(nome_add.toString()).set({
-        nome: nome_add,
-        tema: tema_add,
-        url: url_add,
-        css: css_add,
-        author: firebase.auth().currentUser.uid,
-        time: firebase.timestamp.now()
-    }) .then(() => {
-        console.log("Aruivo adicionado com sucesso!");
-    }) .catch((error) => {
-        console.log("Erro ao adicionar arquivo!");
-        var errorCode = error.code;
-        var errorMessage = error.message;
-    })
+/* ------------------ PUBLIC FUNCTIONS ------------------- */
+function btnTest() {
+    alert('Botão Funcionando sem Evento de LOGIN');
+}
+
+function downloadExtension() {
+    alert('Redirecionar e Iniciar download da extensao')
+}
+
+function downloadTheme() {
+    alert('Iniciar donwload do tema e atualizar extensao')
 }
