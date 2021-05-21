08dfaac5a6a66fbec0731cb1fff99e936a5bc27e
Lucas Paskua <lucas.paskua@gmail.com>
2021-05-20 21:04:55 -0300

    acrescimo da funçao isUserEqual no Api2.js

--- website/Site_projeto/Api2.js
+++ website/Site_projeto/Api2.js
@@ -48,3 +48,17 @@ function onSignIn(googleUser) {
   });
 }
 // [END auth_google_callback]
+
+function isUserEqual(googleUser, firebaseUser) {
+  if (firebaseUser) {
+    var providerData = firebaseUser.providerData;
+    for (var i = 0; i < providerData.length; i++) {
+      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+        // We don't need to reauth the Firebase connection.
+        return true;
+      }
+    }
+  }
+  return false;
+}
