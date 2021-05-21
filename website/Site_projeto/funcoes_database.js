dccb3e85c22a27ef33ba8e2cb13835e0a101685f
Felipe Sato <43546131+Felipe-Sato@users.noreply.github.com>
2021-05-21 13:16:13 -0300

    Correção da Função INSERT

--- website/Site_projeto/funcoes_database.js
+++ website/Site_projeto/funcoes_database.js
@@ -1,13 +1,7 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCN7aowFxK1dkqJ5JWPB-mQ5CwAis5A-9k",
-    authDomain: "manual-dark-mode-14b3f.firebaseapp.com",
-    projectId: "manual-dark-mode-14b3f",
-    storageBucket: "manual-dark-mode-14b3f.appspot.com",
-    messagingSenderId: "358791318355",
-    appId: "1:358791318355:web:a4dc9dc21f81547a56afa6",
-    measurementId: "G-899R097H6V"
-  };
 /* ---------- INITIALIZING FIRESTORE ----------- */
+import firebaseConfig from "../config/firebase-config.json";
+// Initialize Cloud Firestore through Firebase
+let require 
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
 
@@ -18,75 +12,29 @@ var doc_url = document.getElementById("url");
 var doc_css = document.getElementById("codigo");
 
 /* ---------------- USER AUTHENTICATION ------------------ */
-// Login Request
-function loginRequest() {
+// User Only Functions - INSERT
+function insert(doc_name) {
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
             var author = firebase.Auth().currentUser;
-            
-            // User Only Functions - INSERT
-            function insert(doc_name) {
-                return db.collection("Library").doc(doc_name.toString()).set({
-                    nome: doc_name,
-                    tema: doc_filter,
-                    url: doc_url,
-                    css: doc.css,
-                    author: author.uid(),
-                    time: firebase.timestamp.now()
-                })  .then (() => {
-                    console.log("Arquivo criado com sucesso!");
-                }) .catch ((error) => {
-                    console.log("Erro ao criar o arquivo!");
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    console.log('Codigo do erro: ', +errorCode);
-                })
-            }
-
-            // User Only Functions - UPDATE
-            function update(doc_name) {
-                return db.collection("Library").doc(doc_name.toString()).where("author", "==", author.uid).update({
-                    nome: doc_name,
-                    tema: doc_filter,
-                    url: doc_url,
-                    css: doc_css,
-                    author: firebase.auth().currentUser.uid,
-                    time: firebase.timestamp.now()
-                }) .then (() => {
-                    console.log("Arquivo atualizado com sucesso!");
-                }) .catch ((error) => {
-                    console.log("Erro ao atualizar o arquivo!");
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    console.log('Codigo do erro: ', +errorCode);
-                })
-            }
-
-            // User Only Functions - REMOVE
-            function remove(doc_name) {
-                db.collection("Library").doc(doc_name.toString()).where("author", "==", author.uid).delete()
-                . then (() => {
-                    console.log("Arquivo removido com sucesso!");
-                }) .catch ((error) => {
-                    console.log("Erro ao remover arquivo!");
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    console.log('Codigo do erro: ', +errorCode);
-                })
-            }
+            return db.collection("Library").doc(doc_name.toString()).set({
+                nome: doc_name,
+                tema: doc_filter,
+                url: doc_url,
+                css: doc.css,
+                author: author.uid(),
+                time: firebase.timestamp.now()
+            })  .then (() => {
+                console.log("Arquivo criado com sucesso!");
+            }) .catch ((error) => {
+                console.log("Erro ao criar o arquivo!");
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log('Codigo do erro: ', +errorCode);
+            })            
+        } else {
+            alert("Usuário não autenticado");
         }
-    })
-}
-
-// Logout Request
-function logoutRequest() {
-    firebase.auth().signOut()
-    . then (() => {
-        console.log("Logout realizado com sucesso!");
-    }) .catch ((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log('Codigo do erro: ', +errorCode);
     })
 }
 
